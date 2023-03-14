b3b4699cc7e78045f1894734e3b5564465297db5
Max <maks_orlov_2015@mail.ru>
2023-03-14 21:21:21 +1000

    не растет

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
+from utils.utils import data_file, get_filtered_operation
 
 OPERATIONS_PATH = os.path.join('operations.json')
 
-def main():
+user_operation = data_file(OPERATIONS_PATH)
+operation_execut = get_filtered_operation(user_operation)
+
+print(operation_execut)
 
 
-
-
-if __name__ == '__main__':
-    main()
--- utils/utils.py
+++ utils/utils.py
@@ -2,14 +2,14 @@ import json
 
 def data_file(path):
     '''получаем  словарь с json файла'''
-    with open (path, 'r', encoding='utf-8') as file:
+    with open(path, 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
-def get_filtered_operation(data):
+def get_filtered_operation(file):
     '''фильтруем имеющийся список по наличию значения EXECUTED'''
     result = []
-    for item in data:
+    for item in file:
         if item['state'] == 'EXECUTED':
             result.append(item)
     return result

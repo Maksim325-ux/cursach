0215d3fb27bab00d6bce0d45aaec65fad3088acb
Max <maks_orlov_2015@mail.ru>
2023-03-18 12:15:36 +1000

    продолжение

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,8 @@
-from utils.utils import get_filtered_operation, get_last_data, get_format_time
 import pytest
+
+import utils.utils
+from utils.utils import get_filtered_operation, get_last_data, get_format_time, get_data_file, operations
+
 
 @pytest.fixture
 def test_data():
@@ -17,4 +20,10 @@ def test_get_format_time():
     data = get_format_time('2019-08-26T10:50:58.294041')
     assert data == '26.08.2019'
 
-def test_operation():
+def test_get_data_file():
+    data = get_data_file('test.json')
+    assert data == [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041', 'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'}, {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364', 'operationAmount': {'amount': '8221.37', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'MasterCard 7158300734726758', 'to': 'Счет 35383033474447895560'}]
+
+def test_operations():
+    data = operations({'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041', 'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'})
+    assert data == 'Maestro 1596 83** **** 5199 -> Счет **9589'
